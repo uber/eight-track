748cedb56431cabb3c92223100f02e9c8d86cdea
Todd Wolfson <todd@twolfson.com>
2014-01-27 13:07:41 -0800

    Truncate urls in filenames to 32 characters. Fixes #7
    Merge branch 'master' into bug/cannot.save.long.names
    Moved to 32 character truncation as discussed in #7
    Completed test for ENAMETOOLONG regression

--- test/eight-track_test.js
+++ test/eight-track_test.js
@@ -340,6 +340,30 @@ describe('A server that echoes HTTP headers', function () {
   });
 });
 
+// DEV: This is a regression test for https://github.com/uber/eight-track/issues/7
+describe('A server being proxied by `eight-track', function () {
+  serverUtils.run(1337, function (req, res) {
+    res.send('oh hai');
+  });
+  serverUtils.runEightServer(1338, {
+    fixtureDir: __dirname + '/actual-files/basic',
+    url: 'http://localhost:1337'
+  });
+
+  describe('when requested with a very long URL', function () {
+    var lotsOfCharacters = (new Array(9001)).join('a');
+    httpUtils.save('http://localhost:1338/abc/' + lotsOfCharacters + '/def');
+
+    describe('and when requested with a slightly different long URL', function () {
+      httpUtils.save('http://localhost:1338/abc/' + lotsOfCharacters + '/deg');
+
+      it('recognizes it as a separate response', function () {
+        expect(this.requests[1337]).to.have.property('length', 2);
+      });
+    });
+  });
+});
+
 describe('A server with distinct responses', function () {
   serverUtils.run(1337, function (req, res) {
     express.urlencoded()(req, res, function (err) {
