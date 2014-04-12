8320236c18e080a716b3de74bffe912f6485d1cc
Todd Wolfson <todd@twolfson.com>
2014-04-12 12:34:27 -0700

    Copied over test that was lost during merge conflict

--- test/eight-track_test.js
+++ test/eight-track_test.js
@@ -570,3 +570,51 @@ describe('A server being proxied by `eight-track` with a noramlizeFn that modifi
     });
   });
 });
+
+// DEV: This is a regression test for https://github.com/uber/eight-track/issues/17
+describe('A server being proxied by `eight-track` that delivers binary content', function () {
+  serverUtils.run(1337, function (req, res) {
+    var buff = new Buffer(256);
+    buff.write('\u0042', 0);
+    res.send(buff);
+  });
+  serverUtils.runEightServer(1338, {
+    fixtureDir: __dirname + '/actual-files/basic',
+    url: 'http://localhost:1337'
+  });
+
+  describe('when requested', function () {
+    httpUtils.save({
+      encoding: null,
+      url: 'http://localhost:1338/'
+    });
+
+    it('does not encounter an error', function () {
+      // DEV: If you see HPE_INVALID_CONSTANT, it is probably related to Content-Length due to too long of a stringify
+      expect(this.err).to.equal(null);
+    });
+    it('replies with the binary content', function () {
+      expect(this.res.statusCode).to.equal(200);
+      expect(this.body[0]).to.equal(66); // 42 from hex to decimal
+      expect(this.body.length).to.equal(256);
+    });
+
+    describe('and when requested again', function () {
+      httpUtils.save({
+        encoding: null,
+        url: 'http://localhost:1338/'
+      });
+
+      it('replies with the binary content', function () {
+        expect(this.err).to.equal(null);
+        expect(this.res.statusCode).to.equal(200);
+        expect(this.body[0]).to.equal(66); // 42 from hex to decimal
+        expect(this.body.length).to.equal(256);
+      });
+
+      it('does not double request', function () {
+        expect(this.requests[1337]).to.have.property('length', 1);
+      });
+    });
+  });
+});
