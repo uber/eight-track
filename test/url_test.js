b42050c9e41bdb4d65a15b6a5e5315d409a282ef
Todd Wolfson <todd@twolfson.com>
2014-04-18 15:36:54 -0700

    Decided to not introduce complication with altering paths

--- test/url_test.js
+++ test/url_test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var httpUtils = require('./utils/http');
 var serverUtils = require('./utils/server');
 
-describe.only('An `eight-track` server proxying a subpath', function () {
+describe('An `eight-track` server proxying a subpath', function () {
   serverUtils.run(1337, function (req, res) {
     res.send(req.url);
   });
@@ -21,26 +21,7 @@ describe.only('An `eight-track` server proxying a subpath', function () {
   });
 });
 
-describe.skip('An `eight-track` server proxying a `/` subpath', function () {
-  serverUtils.run(1337, function (req, res) {
-    res.send(req.url);
-  });
-  serverUtils.runEightServer(1338, {
-    fixtureDir: __dirname + '/actual-files/redirect',
-    url: 'http://localhost:1337/'
-  });
-
-  describe('when requested with a path', function () {
-    httpUtils.save('http://localhost:1338/world');
-
-    it('uses the normal path', function () {
-      expect(this.err).to.equal(null);
-      expect(this.body).to.equal('/world');
-    });
-  });
-});
-
-describe.skip('An `eight-track` server proxying an HTTPS server', function () {
+describe.only('An `eight-track` server proxying an HTTPS server', function () {
   // TODO: Make this an HTTPS server
   serverUtils.run(1337, function (req, res) {
     res.send('oh hai');
